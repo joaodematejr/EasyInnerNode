d582968347178ffe1980a1818ad78bfe3280d655
João Dematé Jr <joaodematejr@gmail.com>
2022-02-14 13:41:18 -0300

    Versionamento do projeto

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+var ffi = require("ffi-napi");
+var ref = require("ref-napi");
+var struct = require("ref-struct-di");
+var array = require("ref-array-napi");
+var finalize = require("finalize");
+
+async function main() {
+  console.log("aki");
+}
+
+main();
